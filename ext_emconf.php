35e84ee765e9e75336ada7fce94ff13517edf59d
Franz Holzinger <franz.holzinger@festwein.de>
2022-05-23 17:46:42 +0200

    * version 3.0.0 of simple-cache: Support for TYPO3 11.5 and PHP 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Cache Interface',
     'description' => 'Common interfaces for simple caching psr/simple-cache.',
     'category' => 'misc',
-    'version' => '0.0.3',
+    'version' => '3.0.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -18,8 +18,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.3.99',
-            'typo3' => '6.2.0-9.5.99'
+            'php' => '8.0.0-8.99.99',
+            'typo3' => '10.4.0-11.5.99'
         ],
         'conflicts' => [
         ],
