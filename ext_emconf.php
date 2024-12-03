c5410238264b968df91e5dbae3277b213c6da5aa
Franz Holzinger <franz.holzinger@festwein.de>
2024-12-03 13:10:37 +0100

    Support for TYPO3 13

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Cache Interface',
     'description' => 'Common interfaces for simple caching psr/simple-cache.',
     'category' => 'misc',
-    'version' => '3.0.1',
+    'version' => '3.0.2',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '8.0.0-8.99.99',
-            'typo3' => '10.4.0-12.4.99'
+            'typo3' => '11.5.0-13.4.99'
         ],
         'conflicts' => [
         ],
